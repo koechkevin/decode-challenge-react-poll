21d59d46cdd6d8713a2a670e01f2ad0ee2b9907a
Kevin Koech <koechkevin92@gmail.com>
2020-08-29 00:13:50 +0300

    Build static version of the comonents

--- components/Poll.tsx
+++ components/Poll.tsx
@@ -1,17 +1,50 @@
-import * as React from 'react';
+import React, { FC } from 'react';
 import styled from 'styled-components';
-import { QandAsDocument } from '../types';
-import { FC } from 'react';
+import { QandAsDocument, QandA } from '../types';
+import Question from './Question/Question';
 
 type Props = {
   qAndAs: QandAsDocument;
 };
 
 const PollWrapper = styled.div``;
+const Paragraph = styled.p`
+  font-size: 16px;
+`;
+const Page = styled.div`
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+`;
+
+const Flex1 = styled.div`
+  flex: 1;
+  margin: auto;
+  display: flex;
+  align-items: center;
+`;
 
 const Poll: FC<Props> = ({ qAndAs }) => {
-  console.log('questions and answers: ', qAndAs);
-  return <PollWrapper>The Poll implementation goes here</PollWrapper>;
+  const { questions } = qAndAs;
+  return (
+    <PollWrapper>
+      {questions.map((question: QandA, idx: number) => (
+        <Page key={idx}>
+          <Paragraph>
+            Here some text that is on the page in a paragraph tag. The poll will
+            appear within the context below.
+          </Paragraph>
+          <Flex1>
+            <Question questionAndAnswer={question} />
+          </Flex1>
+          <Paragraph>
+            Here is the rest of the text in the page. We just have something
+            down here for context to see it in.
+          </Paragraph>
+        </Page>
+      ))}
+    </PollWrapper>
+  );
 };
 
-export default Poll
+export default Poll;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,15 @@ import GlobalStyles from '../components/GlobalStyles';
 import questions from '../questions.json';
 import Poll from '../components/Poll';
 
-const IndexPage = styled.div``;
+const IndexPage = styled.div`
+ 
+`;
 
 interface Props {}
 
 const Index: FC<Props> = () => (
   <IndexPage>
     <GlobalStyles />
-    Setup Complete!
     <Poll qAndAs={questions} />
   </IndexPage>
 );
